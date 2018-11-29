a0edce06a0349f784a8d3d2f10b87518338dfa5c
Nelly Duclos <nellyduclos8@gmail.com>
2018-11-29 16:52:12 +0100

    fix ff bug

--- db/migrate/20181129142239_add_monetize_to_consumptions.rb
+++
@@ -1,8 +0,0 @@
-class AddMonetizeToConsumptions < ActiveRecord::Migration[5.2]
- def change
-    change_table :consumptions do |t|
-      t.monetize :liter_price 
-      t.monetize :total_price
-    end
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_29_142239) do
+ActiveRecord::Schema.define(version: 2018_11_21_134009) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -38,10 +38,6 @@ ActiveRecord::Schema.define(version: 2018_11_29_142239) do
     t.datetime "updated_at", null: false
     t.integer "user_id"
     t.integer "car_id"
-    t.integer "liter_price_cents", default: 0, null: false
-    t.string "liter_price_currency", default: "USD", null: false
-    t.integer "total_price_cents", default: 0, null: false
-    t.string "total_price_currency", default: "USD", null: false
   end
 
   create_table "users", force: :cascade do |t|
